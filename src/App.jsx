099cd1ce634775a8665a895af3dffbe5685aed4e
jmhs11 <josemariahs@gmail.com>
2023-03-08 22:02:07 +0100

    Add basic structure and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ProductListPage from "./pages/ProductListPage";
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	const productListLoader = async () => {
+		return fetch("/fake/api/products.json");
+	};
+
+	const productDetailsLoader = async ({ params }) => {
+		return fetch("/fake/api/products.json");
+	};
+
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <ProductListPage />,
+			loader: productListLoader,
+		},
+		{
+			path: "product/:id",
+			element: <ProductDetailsPage />,
+			loader: productDetailsLoader,
+		},
+	]);
+
+	return (
+		<>
+			<Header />
+			<main className="px-2 py-4 md:p-4">
+				<RouterProvider router={router} />
+			</main>
+		</>
+	);
 }
 
 export default App;
